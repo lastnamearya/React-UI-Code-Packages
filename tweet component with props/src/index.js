57d81580fb558e6a2b11bd4d9717f4a5dc0de964
lastnamearya <arya.jigyasu6815@gmail.com>
2018-06-13 10:01:45 +0530

    NameWithHandle & Message Component added

--- tweet component with props/src/index.js
+++ tweet component with props/src/index.js
@@ -6,8 +6,12 @@ function Tweet({ tweet }) {
   return (
     <div className="tweet">
       <Avatar url={tweet.img}/>
+      <div className="content">
+        <NameWithHandle author={tweet.author}/>
+        <Message text={tweet.message}/>
+      </div>
     </div>
-  )
+  );
 }
 
 function Avatar({ url }) {
@@ -16,8 +20,34 @@ function Avatar({ url }) {
   );
 }
 
+function NameWithHandle({ author }) {
+  return (
+    <span className="name-with-handle">
+      <span className="name">
+        {author.name}
+      </span>
+      <span className="handle">
+        {author.handle}
+      </span>
+    </span>
+  );
+}
+
+function Message({ text }) {
+  return (
+    <div className="message">
+      {text}
+    </div>
+  );
+}
+
 var tweetObject = {
-  img: "https://avatars2.githubusercontent.com/u/6664187?s=400&u=eb02e1d29d8454abcefd948bf4d879509d56ef74&v=4"
+  img: "https://avatars2.githubusercontent.com/u/6664187?s=400&u=eb02e1d29d8454abcefd948bf4d879509d56ef74&v=4",
+  message: "Something about Cats",
+  author: {
+    name: "Jigyasu",
+    handle: "@lastnamearya"
+  }
 };
 
 ReactDOM.render(
