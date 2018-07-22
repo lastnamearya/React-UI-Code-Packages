cb287f9abd7cfb3a31fd016b1b6ed56b721fb9a9
lastnamearya <arya.jigyasu6815@gmail.com>
2018-07-23 03:11:07 +0530

    Introductin Redux ~ store in our React App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+
 import Posts from "./components/Post";
 import Postform from "./components/Postform";
 
 import "./App.css";
 
+const store = createStore(() => [], {}, applyMiddleware());
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <Postform />
-        <hr />
-        <Posts />
-      </div>
+      <Provider store={store}>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React</h1>
+          </header>
+          <Postform />
+          <hr />
+          <Posts />
+        </div>
+      </Provider>
     );
   }
 }
